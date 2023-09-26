a5f0f06522958859e1c76e35054f621730551286
Fatima <fatimasultanova.909@gmail.com>
2023-09-26 18:20:23 +0400

    FileReader and BubbleSort

---
+++ src/FileReader.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+
+public class FileReader {
+    public static int [] numbers;
+    public static void readFile() {
+
+        File file = new File("numbers/array");
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file));
+            int size = 0;
+            while (bufferedReader.readLine() != null){
+                size++;
+            }
+
+            int []arr = new int[size];
+
+
+            bufferedReader = new BufferedReader(new java.io.FileReader(file));
+            String line;
+            int index = 0;
+
+            while ((line = bufferedReader.readLine())!=null){
+                int num = Integer.parseInt(line);
+                arr[index] = num;
+                index++;
+            }
+            bufferedReader.close();
+
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+}
--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -5,8 +5,14 @@ import sort.inter.Sorting;
 public class BubbleSort implements Sorting {
     @Override
     public void sort(int[] arr) {
-
-
-
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
