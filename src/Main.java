29e7c6880bc26297ce66c055e1406c8a053b3625
Fatima <fatimasultanova.909@gmail.com>
2023-09-27 22:25:53 +0400

    QuickSort

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import sort.QuickSort;
+import sort.inter.Sorting;
 
 public class Main {
     public static void main(String[] args) {
+        Sorting sorting = new QuickSort();
+        int[] arr = {1,7,2,44,8};
+        sorting.sort(arr);
+
+
 
     }
 }
--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -5,6 +5,34 @@ import sort.inter.Sorting;
 public class QuickSort implements Sorting {
     @Override
     public void sort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
 
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low <= high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+
+        int i = low - 1;
+
+        for (int j = low; j <= high; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, j, ++i);
+            }
+        }
+        return i;
+    }
+
+    private void swap(int[] arr, int j, int i) {
+        int temp = arr[i];
+        arr[j] = arr[i];
+        arr[j] = temp;
     }
 }
