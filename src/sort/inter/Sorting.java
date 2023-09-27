15006a0a4c087ff6e40724805132cf3f3cd6622b
Fatima Sultanova <144486053+fatimasultanova@users.noreply.github.com>
2023-09-27 22:41:00 +0400

    Update Sorting.java
    print array

--- src/sort/inter/Sorting.java
+++ src/sort/inter/Sorting.java
@@ -2,4 +2,15 @@ package sort.inter;
 
 public interface Sorting {
     void sort(int [] arr);
+
+     default void print(int [] arr){
+        System.out.print("{ ");
+        for (int z = 0; z < arr.length; z++) {
+            System.out.print(arr[z]);
+            if (z != arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print(" }");
+    }
 }
