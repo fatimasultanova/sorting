b9933cc663093cefeea9e17932d96fece3353e3b
Fatima <fatimasultanova.909@gmail.com>
2023-09-28 11:40:57 +0400

    SelectionSort

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import sort.MergeSort;
 import sort.QuickSort;
+import sort.SelectionSort;
 import sort.inter.Sorting;
 
 public class Main {
     public static void main(String[] args) {
         int [] arr = FileReaderNumbers.readFile();
-        Sorting sorting = new QuickSort();
+        Sorting sorting = new SelectionSort();
         sorting.sort(arr);
         sorting.print(arr);
 
--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -5,6 +5,18 @@ import sort.inter.Sorting;
 public class SelectionSort implements Sorting {
     @Override
     public void sort(int[] arr) {
+        int i, j, small;
+        int n = arr.length;
+        for (i = 0; i < n-1; i++) {
+            small = i;
 
+            for (j = i+1; j < n; j++) {
+                if (arr[j] < arr[small])
+                    small = j;
+            }
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
